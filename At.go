6c3ab382895c000cc7f662562bd76242c7c1ca8f
firas bourguiba <firasbourguiba98@gmail.com>
2024-01-15 16:33:02 +0100

    ??

---
+++ At.go
@@ -0,0 +1,11 @@
+package netpbm
+
+import "fmt"
+
+func (pbm *PBM) AT(x, y int) bool {
+	if y >= 0 && x < pbm.height && x >= 0 && x < pbm.width {
+		fmt.Print("ton chois  ", pbm.data[y][x])
+		return pbm.data[y][x]
+	}
+	return false
+}
