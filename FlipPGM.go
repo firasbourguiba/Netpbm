cc6f9ea64ba2cafafc8057d2b7e81c3a0f1857e3
firas bourguiba <firasbourguiba98@gmail.com>
2024-01-19 15:31:17 +0100

    flipPGM

---
+++ FlipPGM.go
@@ -0,0 +1,8 @@
+package netpbm
+func (pgm *PGM) Flip() {
+	for i := range pgm.data {
+		for j := 0; j < pgm.width/2; j++ {
+			pgm.data[i][j], pgm.data[i][pgm.width-j-1] = pgm.data[i][pgm.width-j-1], pgm.data[i][j]
+		}
+	}
+}
